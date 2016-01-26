865f369598f604c75ca0c6adc4fffcf603134c67
Larry Li <larryli@qq.com>
2016-01-26 17:37:55 +0800

    add tests

---
+++ tests/PlunarTest.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace Plunar\tests;
+
+use Plunar\Plunar;
+
+class PlunarTest extends \PHPUnit_Framework_TestCase
+{
+    public function testIsValidDate()
+    {
+        $this->assertTrue(Plunar::isValidDate(2016, 1, 26));
+        $this->assertTrue(Plunar::isValidDate(1891, 2, 9));
+        $this->assertTrue(Plunar::isValidDate(2100, 2, 9));
+    }
+
+    /**
+     * @expectedException \Plunar\PlunarException
+     */
+    public function testIsValidDateException()
+    {
+        Plunar::isValidDate(1816, 1, 26);
+    }
+
+    /**
+     * @expectedException \Plunar\PlunarException
+     */
+    public function testIsValidDateExceptionLess()
+    {
+        Plunar::isValidDate(1891, 2, 8);
+    }
+
+    /**
+     * @expectedException \Plunar\PlunarException
+     */
+    public function testIsValidDateExceptionThan()
+    {
+        Plunar::isValidDate(2100, 2, 10);
+    }
+
+    public function testSolarToLunar()
+    {
+        $this->assertEquals(
+            array('一九八四', '八月', '廿七', '甲子', '鼠', '闰十月', array(1984, 8, 27)),
+            Plunar::solarToLunar(1984, 9, 22));
+        $this->assertEquals(
+            array('二零一五', '腊月', '十七', '乙未', '羊', 0, array(2015, 12, 17)),
+            Plunar::solarToLunar(2016, 1, 26));
+        $this->assertEquals(
+            array('一九零零', '冬月', '十一', '庚子', '鼠', '闰八月', array(1900, 12, 11)),
+            Plunar::solarToLunar(1901, 1, 1));
+        $this->assertEquals(
+            array('一九九九', '冬月', '廿五', '己卯', '兔', 0, array(1999, 11, 25)),
+            Plunar::solarToLunar(2000, 1, 1));
+    }
+}
